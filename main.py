d7ae02ba79a71cb14a148c6356b86f96c4fede34
Jie Tao <taoj@mail.gvsu.edu>
2018-03-16 16:26:28 -0400

    v0.21
    add linux path

--- main.py
+++ main.py
@@ -2,10 +2,17 @@ from BayersDict import BayerDict
 from multiprocessing import Process
 
 def main():
-    pos_inputpath = "D:\\minitest\\aclImdb\\train\\pos\\"   # training folder for positive
-    pos_outpath = "D:\\minitest\\aclImdb\\pos_output.csv"   # positive output
-    neg_inputpath = "D:\\minitest\\aclImdb\\train\\neg\\"   # training folder for negative
-    neg_outpath = "D:\\minitest\\aclImdb\\neg_output.csv"   # negative output
+    # windows
+    # pos_inputpath = "D:\\minitest\\aclImdb\\train\\pos\\"   # training folder for positive
+    # pos_outpath = "D:\\minitest\\aclImdb\\pos_output.csv"   # positive output
+    # neg_inputpath = "D:\\minitest\\aclImdb\\train\\neg\\"   # training folder for negative
+    # neg_outpath = "D:\\minitest\\aclImdb\\neg_output.csv"   # negative output
+    # linux
+    pos_inputpath = "/home/jie/Documents/aclImdb/train/pos/"   # training folder for positive
+    pos_outpath = "/home/jie/Documents/aclImdb/pos_output.csv"  # positive output
+    neg_inputpath = "/home/jie/Documents/aclImdb/train/neg/"   # training folder for negative
+    neg_outpath = "/home/jie/Documents/aclImdb/neg_output.csv"   # negative output
+
     aBayerdict = BayerDict()
     p_neg = Process(target=aBayerdict.readfile, args=(neg_inputpath, "N",neg_outpath, "w"))
     p_pos = Process(target=aBayerdict.readfile, args=(pos_inputpath, "P",pos_outpath, "w"))
